fcb606b5fc47d78b0d3cc32e33799a4821cae152
wesley mbate <wmbate@meliora.tech>
2023-06-12 11:32:17 +0300

    docs ready for deployment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,8 +10,6 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
-import { ListComponent } from './components/blogs/list/list.component';
-import { DetailComponent } from './components/blogs/detail/detail.component';
 import { BlogListComponent } from './components/blogs/blog-list/blog-list.component';
 import { BlogDetailComponent } from './components/blogs/blog-detail/blog-detail.component';
 import { PodcastDetailComponent } from './components/podcasts/podcast-detail/podcast-detail.component';
@@ -23,8 +21,6 @@ import { PodcastListComponent } from './components/podcasts/podcast-list/podcast
     NavbarComponent,
     FooterComponent,
     HomeComponent,
-    ListComponent,
-    DetailComponent,
     BlogListComponent,
     BlogDetailComponent,
     PodcastDetailComponent,
