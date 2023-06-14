1eb7c15eab1c3a55d857a6f10720d955ab48cd5f
wesley mbate <wmbate@meliora.tech>
2023-06-14 17:35:54 +0300

    added more podcast length to podcast listing

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,12 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
+import { style, animate, transition, trigger } from '@angular/animations';
+
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [   // :enter is alias to 'void => *'
+        style({ opacity: 0 }),
+        animate(500, style({ opacity: 1 }))
+      ])
+    ])
+  ]
+
 })
 export class NavbarComponent implements OnInit {
+  changeIcon = false;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -14,6 +27,11 @@ export class NavbarComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+  }
+
+
+  toggleIcon(): void {
+    this.changeIcon = !this.changeIcon;
   }
 
 }
